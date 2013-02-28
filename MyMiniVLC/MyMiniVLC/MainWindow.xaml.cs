fc25d9eb593f1351a11528bef5bed763a8562655
ayrtoogle <ayrtoogle@gmail.com>
2013-02-28 12:30:17 +0100

    My mini vlc

---
+++ MyMiniVLC/MyMiniVLC/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace MyMiniVLC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static RoutedCommand fileDialogRoutedCommand = new RoutedCommand();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sliderTime.IsEnabled = false;
+        }
+
+
+        public void ExecutedCustomCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            // Video files (*.avi, *.mp4, *.wmv)|*.avi; *.mp4; *.wmv
+            ofd.Filter = "All files (*.*)|*.*";
+            try
+            {
+                if (ofd.ShowDialog() == true)
+                {
+                    MessageBox.Show(ofd.FileName);
+                    videoPlayer.Source = new Uri(ofd.FileName);        
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonPause_Click(object sender, RoutedEventArgs e)
+        {
+
+            videoPlayer.Pause();
+            //MessageBox.Show(videoPlayer.NaturalDuration.ToString());
+            //videoPlayer.Position = TimeSpan.FromSeconds(3600);
+            //sliderTime.Maximum = videoPlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
+            //sliderTime.Value = videoPlayer.Position.TotalMilliseconds;
+        }
+        //http://msdn.microsoft.com/en-us/library/ms748248.aspx
+        private void buttonPlay_Click(object sender, RoutedEventArgs e)
+        {
+       //     sliderTime.Maximum = videoPlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
+            try
+            {
+                videoPlayer.Play();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+            sliderTime.IsEnabled = true;
+            //if (videoPlayer.time
+        }
+
+    }
+}
