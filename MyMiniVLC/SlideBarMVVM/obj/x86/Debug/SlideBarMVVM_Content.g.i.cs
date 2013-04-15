fdd5382767bfd52365394ed04d1b10c84f88807c
Flo <florian.guiho@epitech.eu>
2013-04-15 19:15:16 +0200

    Modification de l'icône du player

--- MyMiniVLC/SlideBarMVVM/obj/x86/Debug/SlideBarMVVM_Content.g.i.cs
+++ MyMiniVLC/SlideBarMVVM/obj/x86/Debug/SlideBarMVVM_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/open.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wmp2.ico")]
 
 
